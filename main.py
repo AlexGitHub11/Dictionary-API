327f1a343d98cecf70d21e8e3948f14cbc3d089b
Alex <richfordalex@gmail.com>
2024-10-21 17:12:27 +0100

    add searchable dictionary

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<word>")
+def api(word):
+    df = pd.read_csv("dictionary.csv")
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+    result = {"word": word, "definition": definition}
+    return result
+
+if __name__ == "__main__":
+    app.run(debug=True, port=4000)
